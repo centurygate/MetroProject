33e9642538fb97a22d0e67b8b6c61613678e437b
centurygate <centurygate@163.com>
2017-02-23 10:58:58 +0800

    [MOD]1、修改bat文件。

--- run.bat
+++ run.bat
@@ -5,6 +5,10 @@ call mvn clean package
 C:
 cd C:\Program Files\apache-tomcat-9.0.0.M13\bin
 call shutdown.bat
+
+
+:: 等待6秒等tomcat进程结束完毕，否则del会报文件被进程占用无法删除
+ping /n 6 127.1>nul
 cd C:\Program Files\apache-tomcat-9.0.0.M13\webapps
 del  MetroProject.war
 rmdir /s /q MetroProject
